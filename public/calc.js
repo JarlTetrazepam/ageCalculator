4fa76a7dc1841426ecb70badf9e9c9e51d6b1012
JarlTetrazepam <gaterzoom3638@gmail.com>
2021-03-04 15:00:48 +0100

    initialized firebase integration, ready to host

---
+++ public/calc.js
@@ -0,0 +1,22 @@
+const submit = document.getElementById("submit")
+const outputWrapper = document.getElementById("outputWrapper")
+const output = document.getElementById("output")
+
+function calcAgeByC14(at, mass) {
+    let atInMin = at / 60
+    let atPerMass = !mass ? atInMin : (mass * 1000) / atInMin
+    let a0 = 13.56
+    let th = 5730
+    let age = Math.round((-Math.log(atPerMass/a0) * (th/Math.log(2))) * 1000) / 1000
+    if (at <= 0) {
+        age = "an infinite amount of"
+    }
+    if (age < 0) return "Error: age can't be negative. Check your inputs"
+    return `The object is ${age} years old.`
+}
+
+submit.addEventListener("click", function() {
+    const activityInputVal = document.getElementById("activity").value
+    const massInputVal = document.getElementById("mass").value
+    output.innerHTML = calcAgeByC14(activityInputVal, massInputVal)
+})
