e19f62407c5c3204018268057061160a8c9f7a02
JarlTetrazepam <gaterzoom3638@gmail.com>
2021-03-04 14:07:03 +0100

    reworked dom and error management

--- calc.js
+++ calc.js
@@ -8,14 +8,15 @@ function calcAgeByC14(at, mass) {
     let a0 = 13.56
     let th = 5730
     let age = Math.round((-Math.log(atPerMass/a0) * (th/Math.log(2))) * 1000) / 1000
-    if (at <= 0) return "an infinite amount of"
-    if (age < 0) return RangeError
-    return age
+    if (at <= 0) {
+        age = "an infinite amount of"
+    }
+    if (age < 0) return "Error: age can't be negative"
+    return `The object is ${age} years old.`
 }
 
 submit.addEventListener("click", function() {
     const activityInputVal = document.getElementById("activity").value
     const massInputVal = document.getElementById("mass").value
-    outputWrapper.style.opacity = 1
     output.innerHTML = calcAgeByC14(activityInputVal, massInputVal)
 })
