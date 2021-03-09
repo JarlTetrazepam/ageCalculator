3a08af518da52c09d09cd58d558608bb97bee901
JarlTetrazepam <gaterzoom3638@gmail.com>
2021-03-09 11:35:41 +0100

    changed some vars to consts

--- public/calc.js
+++ public/calc.js
@@ -3,10 +3,10 @@ const outputWrapper = document.getElementById("outputWrapper")
 const output = document.getElementById("output")
 
 function calcAgeByC14(at, mass) {
-    let atInMin = at * 60
-    let atPerMass = !mass ? atInMin : (mass * 1000) / atInMin
-    let a0 = 13.56
-    let th = 5730
+    const atInMin = at * 60
+    const atPerMass = !mass ? atInMin : (mass * 1000) / atInMin
+    const a0 = 13.56
+    const th = 5730
     let age = Math.round((-Math.log(atPerMass/a0) * (th/Math.log(2))) * 1000) / 1000
     if (at <= 0) {
         age = "an infinite amount of"
